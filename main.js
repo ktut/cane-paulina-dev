2cf80cd58572d0f5240c24a26b2e0f9a6cab6488
Ramzi Dreessen <ramzidreessen@gmail.com>
2018-09-29 14:12:58 -0500

    basic layout

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
-console.log("test!");
+// fade in items as necessary
+$('.fadein').addClass('fadedin');
+
+// slide up tables on load
+$('table').slideUp();
+
+// toggle available units sliding
+$(document).on("click",".unit-dropdown",function() {
+    $( this ).next("table").slideToggle();
+});
