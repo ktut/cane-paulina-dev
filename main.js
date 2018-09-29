a56c3b370baa84e43e0440d5831df3dac52f3cb4
Ramzi Dreessen <ramzidreessen@gmail.com>
2018-09-29 15:17:20 -0500

    made responsive

--- main.js
+++ main.js
@@ -8,3 +8,9 @@ $('table').slideUp();
 $(document).on("click",".unit-dropdown",function() {
     $( this ).next("table").slideToggle();
 });
+
+//Smooth scrolling with links
+$('a[href*=\\#]').on('click', function(event){     
+    event.preventDefault();
+    $('html,body').animate({scrollTop:$(this.hash).offset().top}, 500);
+});
