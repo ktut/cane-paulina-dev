da0451707b52bc5f9400c1d4913a4e8450f90638
Ramzi Dreessen <ramzidreessen@gmail.com>
2018-09-29 17:08:01 -0500

    no idea why slidetoggle isn't working

--- main.js
+++ main.js
@@ -9,7 +9,7 @@ $( document ).ready(function() {
 });
 
 // toggle available units sliding
-$(document).on("click",".unit-dropdown",function() {
+$(document).on("click",".unit-dropdown.active",function() {
     $( this ).next("div.table").slideToggle();
 });
 
